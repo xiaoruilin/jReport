5d36a8395db13a8af0e876e4fd487db662512ec7
xiaoruilin <xiaoruilin@outlook.com>
2019-06-21 18:05:05 +0800

    C#调用

---
+++ webclientnetcall/webclientnetcall/webclientnetcall/Index.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webclientnetcall
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Credentials = CredentialCache.DefaultCredentials;
+                wc.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                wc.QueryString["systemId"] = "fffff";
+                byte[] fileb = wc.DownloadData(new Uri("http://127.0.0.1:8080/hei"));
+
+                //byte[] fileb = wc.UploadFile(new Uri("http://127.0.0.1:8080/hei"), "POST");
+                string res = Encoding.GetEncoding("gb2312").GetString(fileb);
+            }
+        }
+    }
+}
